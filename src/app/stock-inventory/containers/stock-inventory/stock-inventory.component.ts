807ad31e9d721e6fc7303e7e413d92f1570c720c
Roberto Gomez <RobertoGo@protonmail.com>
2020-10-28 13:33:19 +0100

    Reactive Forms - FormControls and FormGroups

--- src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
+++ src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -1,14 +1,48 @@
 import { Component } from '@angular/core';
+import { from } from 'core-js/fn/array';
+import { FormControl, FormGroup, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'stock-inventory',
   styleUrls: ['stock-inventory.component.scss'],
   template: `
     <div class="stock-inventory">
-      Hello World!
+      <form [formGroup]="form" (ngSubmit)="onSubmit()">
+        <div formGroupName="store">
+          <input
+            type="text"
+            placeholder="Branch ID"
+            formControlName="branch">
+
+          <input
+            type="text"
+            placeholder="Manager Code"
+            formControlName="code">
+        </div>
+
+        <div class="stock-inventory__buttons">
+          <button
+            type="submit"
+            [disabled]="form.invalid">
+            Order stock
+          </button>
+        </div>
+
+        <pre>{{ form.value | json }}</pre>
+
+      </form>
     </div>
   `
 })
 export class StockInventoryComponent {
-  constructor() {}
+  form = new FormGroup({
+    store: new FormGroup({
+      branch: new FormControl('B182'),
+      code: new FormControl('1234')
+    })
+  })
+
+  onSubmit() {
+    console.log('Submit:', this.form.value);
+  }
 }
