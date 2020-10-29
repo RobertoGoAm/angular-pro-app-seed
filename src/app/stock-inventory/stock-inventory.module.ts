90bcbad52661a5cc8f97f9212c5163dba0aeb638
Roberto Gomez <RobertoGo@protonmail.com>
2020-10-29 14:28:59 +0100

    Reactive Forms - Componetizing FormGroups

--- src/app/stock-inventory/stock-inventory.module.ts
+++ src/app/stock-inventory/stock-inventory.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { StockInventoryComponent } from './containers/stock-inventory/stock-inventory.component';
+import { StockBranchComponent } from './component/stock-branch/stock-branch.component';
+import { StockProductsComponent } from './component/stock-products/stock-products.component';
+import { StockSelectorComponent } from './component/stock-selector/stock-selector.component';
 
 @NgModule({
   declarations: [
-    StockInventoryComponent
+    StockInventoryComponent,
+    StockBranchComponent,
+    StockProductsComponent,
+    StockSelectorComponent
   ],
   imports: [
     CommonModule,
