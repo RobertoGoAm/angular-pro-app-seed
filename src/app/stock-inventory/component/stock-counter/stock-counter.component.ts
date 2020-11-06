fc23388e37aafdf5343a72fe38670ea1b820d1a9
Roberto Gomez <RobertoGo@protonmail.com>
2020-11-06 13:48:14 +0100

    Reactive Forms - Implementing a ControlValueAccessor

--- src/app/stock-inventory/component/stock-counter/stock-counter.component.ts
+++ src/app/stock-inventory/component/stock-counter/stock-counter.component.ts
@@ -1,7 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+const COUNTER_CONTROL_ACCESSOR = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => StockCounterComponent),
+  multi: true
+};
 
 @Component({
   selector: 'stock-counter',
+  providers: [COUNTER_CONTROL_ACCESSOR],
   styleUrls: ['stock-counter.component.scss'],
   template: `
     <div class="stock-counter">
@@ -29,22 +37,41 @@ import { Component, Input } from '@angular/core';
     </div>
   `
 })
-export class StockCounterComponent {
+export class StockCounterComponent implements ControlValueAccessor {
   @Input() step: number = 10;
   @Input() min: number = 10;
   @Input() max: number = 1000;
-
   value: number = 10;
+  private onTouch: Function;
+  private onModelChange: Function;
+
+  writeValue(value: number) {
+    this.value = value || 0;
+  }
+
+  registerOnChange(fn: Function) {
+    this.onTouch = fn;
+  }
+
+  registerOnTouched(fn: Function) {
+    this.onModelChange = fn;
+  }
 
   increment() {
     if (this.value < this.max) {
       this.value = this.value + this.step;
+      this.onModelChange(this.value);
     }
+
+    this.onTouch();
   }
 
   decrement() {
     if (this.value > this.max) {
       this.value = this.value - this.step;
+      this.onModelChange(this.value);
     }
+
+    this.onTouch();
   }
 }
