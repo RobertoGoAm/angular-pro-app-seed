862adfbee6347feee99306e12e2654270e4e8c6b
Roberto Gomez <RobertoGo@protonmail.com>
2020-11-13 13:15:19 +0100

    Routing - Dynamic route resolves with snapshots

--- src/app/mail/mail.module.ts
+++ src/app/mail/mail.module.ts
@@ -1,27 +1,27 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule, Routes } from "@angular/router";
 
-import { MailFolderComponent } from './containers/mail-folder/mail-folder.component';
-import { MailItemComponent } from './components/mail-item/mail-item.component';
-import { MailAppComponent } from './components/mail-app/mail-app.component';
+import { MailFolderComponent } from "./containers/mail-folder/mail-folder.component";
+import { MailItemComponent } from "./components/mail-item/mail-item.component";
+import { MailAppComponent } from "./components/mail-app/mail-app.component";
+import { MailService } from "./mail.service";
+import { MailFolderResolve } from "./containers/mail-folder/mail-folder.resolve";
 
 export const ROUTES: Routes = [
-  { path: 'folder/:name', component: MailFolderComponent }
+  {
+    path: "folder/:name",
+    component: MailFolderComponent,
+    resolve: {
+      messages: MailFolderResolve
+    }
+  }
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule.forChild(ROUTES)
-  ],
-  declarations: [
-    MailFolderComponent,
-    MailAppComponent,
-    MailItemComponent
-  ],
-  exports: [
-    MailAppComponent
-  ]
+  imports: [CommonModule, RouterModule.forChild(ROUTES)],
+  declarations: [MailFolderComponent, MailAppComponent, MailItemComponent],
+  exports: [MailAppComponent],
+  providers: [MailService, MailFolderResolve]
 })
 export class MailModule {}
