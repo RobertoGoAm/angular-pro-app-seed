98b2ecb0f9a78dafc2b000772e149f4dae6cda79
Roberto Gomez <RobertoGo@protonmail.com>
2020-11-21 20:10:16 +0100

    Routing - Guards with canDeactivate

--- src/app/mail/components/mail-view/mail-view.component.ts
+++ src/app/mail/components/mail-view/mail-view.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { Observable } from "rxjs/Observable";
 import { Mail } from "../../models/mail.interface";
@@ -10,10 +10,39 @@ import "rxjs/add/operator/pluck";
   template: ` <div class="mail-view">
     <h2>{{ (message | async).from }}</h2>
     <p>{{ (message | async).full }}</p>
+    <div class="mail-reply">
+      <textarea
+        (change)="updateReply($event.target.value)"
+        placeholder="Type your reply..."
+        [value]="reply"
+      >
+      </textarea>
+
+      <button type="button" (click)="sendReply()">Send</button>
+    </div>
   </div>`
 })
-export class MailViewComponent {
+export class MailViewComponent implements OnInit {
   message: Observable<Mail> = this.route.data.pluck("message");
+  reply: string = "";
+  hasUnsavedChanges = false;
 
   constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.params.subscribe(() => {
+      this.reply = "";
+      this.hasUnsavedChanges = false;
+    });
+  }
+
+  updateReply(value: string) {
+    this.reply = value;
+    this.hasUnsavedChanges = true;
+  }
+
+  sendReply() {
+    console.log("Sent!", this.reply);
+    this.hasUnsavedChanges = false;
+  }
 }
--- src/app/mail/mail.module.ts
+++ src/app/mail/mail.module.ts
@@ -11,6 +11,7 @@ import { MailViewComponent } from "./components/mail-view/mail-view.component";
 import { MailViewResolve } from "./components/mail-view/mail-view.resolve";
 import { AuthModule } from "../auth/auth.module";
 import { AuthGuard } from "../auth/auth.guard";
+import { MailViewGuard } from "./components/mail-view/mail-view.guard";
 
 export const ROUTES: Routes = [
   {
@@ -29,6 +30,7 @@ export const ROUTES: Routes = [
         path: "message/:id",
         component: MailViewComponent,
         outlet: "pane",
+        canDeactivate: [MailViewGuard],
         resolve: {
           message: MailViewResolve
         }
@@ -46,6 +48,6 @@ export const ROUTES: Routes = [
     MailViewComponent
   ],
   exports: [MailAppComponent],
-  providers: [MailService, MailFolderResolve, MailViewResolve]
+  providers: [MailService, MailFolderResolve, MailViewResolve, MailViewGuard]
 })
 export class MailModule {}
