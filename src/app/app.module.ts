85ff8cda1c416a43ae423abaedcebf02c02dcb57
Roberto Gomez <RobertoGo@protonmail.com>
2020-11-30 11:02:06 +0100

    Dependency Injection and Zones - Using InjectionToken

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,6 +31,7 @@ import { AuthGuard } from "./auth/auth.guard";
 import { PizzaViewerComponent } from "./containers/pizza-viewer.component";
 import { SideViewerComponent } from "./containers/side-viewer.component";
 import { DrinkViewerComponent } from "./containers/drink-viewer.component";
+import { API_TOKEN } from "./token";
 
 export class CustomPreload implements PreloadingStrategy {
   preload(route: Route, fn: () => Observable<any>): Observable<any> {
@@ -74,7 +75,7 @@ export const ROUTES: Routes = [
   providers: [
     Store,
     CustomPreload,
-    { provide: "api", useValue: "/api/pizzas" }
+    { provide: API_TOKEN, useValue: "/api/pizzas" }
   ],
   bootstrap: [AppComponent]
 })
